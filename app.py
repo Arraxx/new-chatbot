cd0f21166aa8b3fb42bcb48735bc686e6d5ef12a
Aman Kumar <54955755+Amankumar29@users.noreply.github.com>
2020-07-31 18:42:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+import os
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+    
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
