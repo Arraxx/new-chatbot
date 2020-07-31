f0d4ea72411a47d3912a9e4b1117c1cb2734b033
Aman Kumar <54955755+Amankumar29@users.noreply.github.com>
2020-07-31 22:15:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
+from chatbot import chatbot
 import os
+import nltk
 
 app = Flask(__name__)
 app.static_folder = 'static'
