cfd4fc7720a2f30489ddcdaba40e0272c1e5363b
Aman Kumar <54955755+Amankumar29@users.noreply.github.com>
2020-07-31 22:36:30 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
-import nltk
+
 # Creating ChatBot Instancep
 
 chatbot = ChatBot(
